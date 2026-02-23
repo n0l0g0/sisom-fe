fff3822e331d302d2426bf56101bb9a62dcd0309
Sisom Deploy <devops@sisom.local>
2026-02-23 12:19:14 +0100

    chore(deploy): FE auto-commit after docker deploy - 2026-02-23 12:19:14 +0100

--- src/app/bills/[id]/print/page.tsx
+++ src/app/bills/[id]/print/page.tsx
@@ -349,6 +349,7 @@ export function InvoicePrint({
             <div className="font-semibold">ติดต่อสอบถาม</div>
             <div>โทร. 092 426 9477</div>
             <div>Line ID: @sisomoffice</div>
+            <div>เนื่องด้วยหอพักได้เปลี่ยนระบบใหม่ใช้ Line ในการบริหารจัดการ และจะยกเลิกระบบเดิมของ Application Horganice</div>
           </div>
           <div className="w-24 h-24 bg-white p-1 border">
             <img
